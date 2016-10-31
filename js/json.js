ff23d720ab2bd332dcd345830732644784938957
Doug Hawkinson <doug5solas@gmail.com>
2016-10-30 20:54:07 -0700

    Ready to deploy per Customer desire.

--- js/json.js
+++ js/json.js
@@ -1,6 +1,6 @@
-var inventory = [
+var inventorySource = [
     {
-        caption: "Herman Miller Benching",
+        caption: "Herman Miller Benching.",
         manufacturer: "Herman Miller",
         product: "Canvas",
         priPic: "HMbenching.jpg",
@@ -13,7 +13,7 @@ var inventory = [
         description: '<p>Herman Miller Canvas “Benching” solution using a glass topped panel 46” tall.  Work surface is 60”wide x 30” deep.</p>'
     },
     {
-        caption: "Herman Miller Canvas 57",
+        caption: "Herman Miller Canvas 57.",
         manufacturer: "Herman Miller",
         product: "Canvas",
         priPic: "HMcanvas57.jpg",
@@ -22,38 +22,38 @@ var inventory = [
         price: "Please call for quote",
         available: "Now",
         deadline: "While they last",
-        tagline: "Efficient office workstation",
+        tagLine: "Efficient office workstation",
         description: '<p>Herman Miller Canvas with 57” tall panels can make 6x6 or 6x8 stations. 46” tall side panels with glass risers.</p><p>White curved work surfaces, whiteboard tiles and overhead storage</p>'
     },
     {
-        caption: "Canvas 57 with glass",
+        caption: "Herman Miller Canvas 57 w/glass.",
         manufacturer: "Herman Miller",
         product: "Canvas",
         priPic: "HMcanvas57glass.jpg",
-        morePics: ["HMcanvas57glassA.jpg", "HMcanvas57glassB.jpg"],
+        morePics: ["HMcanvas57glassA.jpg"],
         quantity: "521",
         price: "Please call for quote",
         available: "Late November 2016",
         deadline: "While they last",
-        tagline: "",
+        tagLine: "",
         description: '<p>Herman Miller Canvas with 57" high panels, glass tiles in the top position on the aisle.  Two work surfaces in each station:<span>one is curvilinear and measures 36”x30”x36”, the other is 24”x48” straight.</span><p>Each station includes a storage unit with shelves, drawers, lateral file drawers, a box/box/file pedestal and whiteboard tiles.</p><p>Finishes are silver gray, white, tan</p>'
     },
     {
-        caption: "Canvas Bullpen",
+        caption: "Herman Miller Canvas Bullpen.",
         manufacturer: "Herman Miller",
         product: "Canvas",
         priPic: "HMcanvas57bullpen.jpg",
-        morePics: [],
+        morePics: ["HMcanvas57bullpenA.jpg"],
         quantity: "100",
         price: "Please call for quote",
         available: "Now",
         deadline: "While they last",
-        tagline: "",
+        tagLine: "",
         description: '<p>This Herman Miller Canvas can provide co-working groups using mid height panels to create two-person workstations with a shared center opening.</p><p>Each person has either a 6x7 or 8x7 personal workspace with surfaces 30” and 24” deep.</p><p>Each person also has a 3-drawer filing pedestal, a 2-high open shelf and a 2-drawer lateral file (cushions on top are an option.)</p><p>The fabric portion of the panels is 36" tall and the glass stackers bring the panels to 46” tall and 57” tall.</p><p>White worksurfaces, silver gray metal trim and tan fabrics.</p>'
     },
     {
-        caption: "HumanScale Chairs",
-        manufacturer: "Human Scale",
+        caption: "HumanScale Chairs.",
+        manufacturer: "HumanScale",
         product: "Liberty",
         priPic: "humanScaleTask.jpg",
         morePics: [],
@@ -61,12 +61,12 @@ var inventory = [
         price: "Please call for quote",
         available: "Now",
         deadline: "While they last",
-        tagline: "<strong>Volume Discount Available</strong>",
+        tagLine: "Volume Discount Available",
         description: '<p>HumanScale Liberty Desk Chairs:  light gray shell, mesh and seat.  These chairs retail for over $875.00.</p><p>Our price for cleaned, inspected and ready-to-use chairs is $285.00.</p>'
     },
 
     {
-        caption: "BRNO Chairs",
+        caption: "Knoll BRNO Chairs.",
         manufacturer: "Knoll",
         product: "BRNO",
         priPic: "brno01.jpg",
@@ -75,23 +75,30 @@ var inventory = [
         price: "$850/pr",
         available: "Now",
         deadline: "While they last",
-        tagline: "Contemporary office chairs",
+        tagLine: "Contemporary office chairs",
         description: "<p>Heavy contemporary chairs in excellent condition, designed by Mies Van Der Rohe for Knoll.  Fabric has hints of pea green, purple and red, but shows as an overall neutral.  Great for a small lobby, breakout room, or mid-century modern design in the home.</p>"
-    },
-
-    //  extra element for expansion copying
-    {
-        caption: "",
-        manufacturer: "",
-        product: "",
-        priPic: "",
-        morePics: [],
-        quantity: "",
-        price: "Please call for quote",
-        available: "",
-        deadline: "",
-        tagline: "",
-        description: ""
     }
 
 ];
+
+/*
+
+ //  extra element for expansion copying - be sure to:
+ //         place a , after current last }
+ //         and leave it off of new last }
+ ********** copy from below here **********
+ {
+ caption: "",
+ manufacturer: "",
+ product: "",
+ priPic: "",
+ morePics: [],
+ quantity: "",
+ price: "Please call for quote",
+ available: "",
+ deadline: "",
+ tagLine: "",
+ description: ""
+ }
+ ********** copy from above here **********
+ */
--- js/main.js
+++ js/main.js
@@ -1,73 +1,135 @@
 (function () {
     'use strict';	// this function is strict... prevents use of undeclared variables
     /*globals $:false */		//	prevent jshint from declaring $ as undefined
-    console.log ('@ top of JS');
-    $ ('body').scrollspy ({target: '.nav'});
-    $ ('.nav').on ('click', 'li', function (e) {
-        $ ('li').removeClass ("active");         //  remove 'active' class from all links
-        $ (this).addClass ("active");            //  restore 'active' class to link clicked
-        var pageId = $ (this).textContent;       //  identify the "page"
-        $ ('#siteWrapper').removeAttr ('style');
-        $ ('#header').removeAttr ('style');
-        switch (pageId) {
-            case 'about':
-                var bkg = "background-color: var(--ablt-bkg-color);";
-                break;
-            case 'inventory':
-                bkg = "background-color: var(--cilt-bkg-color);";
-                break;
-            case 'contact':
-                bkg = "background-color: var(--cnlt-bkg-color);";
-                break;
-            default:
-                bkg = "background-color: var(--hmlt-bkg-color);";
-        }
-        $ ('#siteWrapper').attr ('style', bkg);
-        $ ('#header').attr ('style', bkg);
 
-    });
+    //  build inventory gallery on load
+    //  NOTE: this is dynamic content - currently sourced from JSON (inventorySource)
+    //  it will likely change to a dB in the future but will never be sourced from the HTML
 
-    $ ('#inventoryContent').on ('click', 'img', function (e) {
-        debugger;
-        $ ("#inventoryModal").empty ();               //  remove any previous HTML from prior modal
-        var i = e.target.id;                          //  id matches "modal-x" where x = numeric index number
-        i = parseInt ((i.split ("-")[1]), 10);        //  parse the index out from the class
-        var inventoryParms = inventory[i];            //  use the index to pull the modal Parameters from json.js
-        //  build modal format
-        var modalHTML = '<!-- The Modal -->';
-        modalHTML += '<div id="modal-popup">';
-        modalHTML += '<button class="close">&times;</button>';
-        modalHTML += '<div id="modal-header" class="tablet"><h2>' + inventoryParms.manufacturer + '<small> (' + inventoryParms.product + ')</small></h2></div>';
-        modalHTML += '<div id="modal-img"><img class="modal-content" src="' + inventoryParms.priPic + '" alt="' + inventoryParms.caption + '"></div>';
-        modalHTML += '<div id="modal-info" class="modal-content">';
-        if (inventoryParms.morePics === []) {
-            modalHTML += '<div> </div>';
-        } else {
-            for (var j = 0; j < inventoryParms.morePics.length; j++) {
-                modalHTML += '<div><img src="images/' + inventoryParms.morePics[j] + '" alt="modal Pic"></div>';
-            }
-        }
-        modalHTML += '<div>Quantity  : ' + inventoryParms.quantity + '</div>';
-        modalHTML += '<div>Available : ' + inventoryParms.available + '</div>';
-        modalHTML += '<div>Deadline  : ' + inventoryParms.deadline + '</div>';
-        modalHTML += '<div>Quantity  : ' + inventoryParms.quantity + '</div>';
-        modalHTML += '<div>Price     : ' + inventoryParms.price + '</div>';
-        modalHTML += '<div>Tag line  : ' + inventoryParms.tagLine + '</div>';
-        modalHTML += '<div>Decription: ' + inventoryParms.description + '</div>';
-        modalHTML += '</div>';
-        $ ("#inventoryModal").append (modalHTML);
-        $ ("#inventoryModal").show ();
-    });
-    // build inventory page
-    console.log ('@ build inventory page');
-    debugger;
-    var invHTML = '';
-    for (var i = 0; i < inventory.length; i++) {
-        invHTML += '<figure id="imgI-' + i + '"><img class="img-responsive cap-top" src="images/' + inventory.priPic + '" alt="' + inventory.caption + '"><figcaption>' + inventory.caption + '</figcaption></figure>';
+    var invHTML = '<div id="inv-gallery" class="row">';     //  row of gallery images (wraps)
+    for (var i = 0; i < inventorySource.length; i++) {
+        invHTML += '<figure class="thumbnail col-xs-6 col-sm-4 col-md-2"><a href="#imgInv-' + i + '" data-toggle="modal"><img src="images/' + inventorySource[i].priPic + '" class="img-responsive cap-top center-block inventory-item" role="button" data-target="#imgInv-' + i + '" alt="' + inventorySource[i].caption + '"></a><figcaption>' + inventorySource[i].caption + ' Click for details.</figcaption></figure>';
+        //invHTML += '<li class="thumbnail col-md-4 col-sm-6"><a href="#imgInv-'+i+'" data-toggle="modal"><img src="images/' + inventorySource[i].priPic + '" class="img-responsive cap-top center-block inventory-item" role="button" data-target="#imgInv-'+i+'" alt="' + inventorySource[i].caption + '"></a><div class="caption">' + inventorySource[i].caption + ' Click for details.</div></li>';
     }
-    invHTML += '<div class="pageDivider"><img class="img-responsive" src="images/dividerSolid.png" alt="divider"></div>';
-    $ ("#inventoryContent").empty ();
-    $ ("#inventoryContent").append (invHTML);
+    invHTML += '</div>';                                    //  close the row of gallery images
+    invHTML += '<div class="row">';                         //  row with divider
+    invHTML += '<img class="col-xs-12 img-responsive" src="images/dividerSolid.png" alt="divider">';
+    invHTML += '</div>';                            //  close the divider row
+    $ ("#inv-gallery").remove ();                     //  probably not needed because this is load time - just caution
+    $ ("#inventory").append (invHTML);               //  append the galery to the page.
 
+    //  end build inventory gallery
 
-});
+    navigationResets ();
+
+    var topoffset = 15;
+
+    //  Activate Scrollspy
+    $ ('body').scrollspy ({
+        target: '.navbar-fixed-top',
+        offset: topoffset
+    });
+
+    //  Add in inBody class to nav when scrollspy event fires
+    $ ('.navbar-fixed-top').on ('activate.bs.scrollspy', function () {
+        navigationResets ()
+    });
+    //Use smooth scrolling when clicking on navigation
+    $ ('.navbar-fixed-top a[href*="#"]:not([href="#"])').click (function () {
+        if (location.pathname.replace (/^\//, '') === this.pathname.replace (/^\//, '') && location.hostname === this.hostname) {
+            var target = $ (this.hash);
+            target = target.length ? target : $ ('[name=' + this.hash.slice (1) + ']');
+            if (target.length) {
+                $ ('html,body').animate ({
+                    scrollTop: target.offset ().top - topoffset + 2
+                }, 500);
+                return false;
+            }                // target.length
+        }                    // click function
+    });                      // smooth scrolling
+
+    function navigationResets () {
+        $ ('.navbar-fixed-top').on ('activate.bs.scrollspy', function () {      //  Clear inBody class to nav on page load
+            var hash = $ (this).find ('li.active a').attr ('href');
+            var pageBkg = '';
+            var headBkg = '';
+            if (hash !== '#home') {
+                $ ('nav').addClass ('inBody');
+                if (hash === '#about') {
+                    pageBkg = 'background-color: var(--ablt-bkg-color)';
+                    headBkg = 'background-color: var(--abac-bkg-color)';
+                } else if (hash === '#inventory') {
+                    pageBkg = 'background-color: var(--cilt-bkg-color)';
+                    headBkg = 'background-color: var(--ciac-bkg-color)';
+                } else {
+                    pageBkg = 'background-color: var(--cnlt-bkg-color)';
+                    headBkg = 'background-color: var(--cnac-bkg-color)';
+                }
+            } else {
+                $ ('nav').removeClass ('inBody');
+                pageBkg = 'background-color: var(--hmlt-bkg-color)';
+                headBkg = 'background-color: var(--hmac-bkg-color)';
+            }
+            $ ('body').attr ('style', pageBkg);                                  //  add page background style to body
+            $ ('nav').attr ('style', headBkg);                                   //  add header background style to nav
+        });
+    }
+
+    $ ('#inv-gallery').on ('click', 'img', function (e) {
+        $ ("#inv-modal").remove ();                                             //  remove any previous HTML from prior modal
+        debugger;
+        var image = e.target.parentElement.hash;                              //  id matches "invImg-x" where x = numeric index number
+        var l = image.length;
+        image = image.substring (1, l);                                       //  strip the leading hashtag
+        var i = parseInt ((image.split ("-")[1]), 10);                         //  parse the index out from the id
+        var inventoryParms = inventorySource[i];                              //  use the index to pull the modal Parameters from json.js
+
+        //  build modal format
+
+        var modalHTML = '<div id="' + image + '" class="modal fade" tabindex="-1">';    //  define the modal
+        modalHTML += '<div class="modal-dialog modal-md">';                               //  define the modal-dialog
+
+        //      modal header -- contains Mfg Name, Mfg Product Line & 'close' button
+        modalHTML += '<div class="modal-header">';
+        modalHTML += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>';
+        modalHTML += '<h4 class="modal-title" id="modalLabel">' + inventoryParms.manufacturer + ' ' + inventoryParms.product + '</h4>';
+        modalHTML += '</div>';                                                                   //  close modal-header
+
+        //      modal body -- contains primary product photo (the image from the gallery) & any (optional) Secondary Pics -- up to 5
+        modalHTML += '<div class="modal-body">';
+        modalHTML += '<img class="img-responsive primaryPic" src="images/' + inventoryParms.priPic + '" alt="' + inventoryParms.caption + '" style="width: 300px; height: auto; padding: 2em;">';
+        if (inventoryParms.morePics.length > 0) {
+            modalHTML += '<div class="secContainer">';                                           //  build div for secondary images
+            for (var j = 0; j < inventoryParms.morePics.length; j++) {
+                modalHTML += '<img class="img-responsive secondaryPic" src="images/' + inventoryParms.morePics[j] + '" alt="modal Pic" style="width: 100px; height: auto; padding: 2em;">';                                                //  include the secondary images
+            }
+            modalHTML += '</div>';                                                              //  close the secContainer
+
+        }
+        modalHTML += '</div>';                                                                  //  close modal-body
+
+        //      modal footer -- contains the inventory particulars (qty, availability, deadline, price, (opt) tag line, description
+        modalHTML += '<div class="modal-footer">';
+        modalHTML += '<p>Quantity   : ' + inventoryParms.quantity + '</p>';
+        modalHTML += '<p>Available  : ' + inventoryParms.available + '</p>';
+        modalHTML += '<p>Deadline   : ' + inventoryParms.deadline + '</p>';
+        modalHTML += '<p>Price      : ' + inventoryParms.price + '</p>';
+        if ((inventoryParms.tagLine !== '') && (inventoryParms.tagLine !== 'undefined')) {
+            modalHTML += '<p><strong>Tag line   : ' + inventoryParms.tagLine + '</strong></p>';
+        }
+        modalHTML += '<div>Description: <span>' + inventoryParms.description + '</span></div>';
+        modalHTML += '</div>';                                                                  //  close modal-footer
+        modalHTML += '</div>';                                                                  //  close the Modal
+
+        $ ("#inventory").append (modalHTML);
+
+        $ ("#inv-modal").modal ('show');
+
+        //  close modal window
+
+        $ (".close").on ('click', 'btn', function () {
+            $ ("#inv-modal").modal ('hide');
+        });
+    });
+
+} ());
